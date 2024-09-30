6d004cc6bb1cafee14f72203b7e27941225be623
charlotte sucias shepherd <charlotte_sucias@hotmail.co.uk>
2024-09-30 12:20:22 +0100

    added recipe seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Recipe.destroy_all
+
+Recipe.create(name: "Paella", description: "A Spanish classic", rating: 10.0)
+Recipe.create(name: "Spagetti Carbonara", description: " An Italian favourite", rating: 8.8)
+Recipe.create(name: "Tikka Massala", description: "A delicious curry", rating: 9.2)
+Recipe.create(name: "Toad in the hole", description: "Hearty winter food", rating: 7.9)
